153f38441cf4eddf73db8ce1d7d2aa67080410cd
Har01d <power0721@gmail.com>
2026-03-21 11:42:09 +0800

    支持百度网盘

--- services/cloud/__init__.py
+++ services/cloud/__init__.py
@@ -4,5 +4,6 @@ Cloud service module.
 
 from .download_service import CloudDownloadService
 from .quark_service import QuarkDriveService
+from .baidu_service import BaiduDriveService
 
-__all__ = ['QuarkDriveService', 'CloudDownloadService']
+__all__ = ['QuarkDriveService', 'BaiduDriveService', 'CloudDownloadService']
